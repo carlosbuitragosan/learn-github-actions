36de590f0184a66d95852a36d305ac251045c6d4
Carlos Buitrago <letscatchupagain@gmail.com>
2024-10-06 11:01:34 +0100

    CH3_01 fixing syntax

--- some-code.py
+++ some-code.py
@@ -1,12 +1,12 @@
 class Calculator:
-    def __init__(self, value=0)
+    def __init__(self, value=0):  # Added colon
         self.value = value
 
     def add(self, x):
         self.value += x
         return self.value
 
-    def subtract(self, x)
+    def subtract(self, x):  # Added colon
         self.value -= x
         return self.value
 
@@ -17,28 +17,28 @@ class Calculator:
     def divide(self, x):
         if x == 0:
             print("Error: Division by zero")
-        else
-            self.value /= x
+        else:
+            self.value /= x  # Fixed indentation of else statement
         return self.value
 
-    def reset(self)
+    def reset(self):  # Added colon
         self.value = 0
         return self.value
 
-calc = Calculator(10
+calc = Calculator(10)  # Added closing parenthesis
 print("Initial value:", calc.value)
 
-calc.add(5
+calc.add(5)  # Added closing parenthesis
 print("After adding 5:", calc.value)
 
-calc.subtract(3
+calc.subtract(3)  # Added closing parenthesis
 print("After subtracting 3:", calc.value)
 
-calc.multiply(2
+calc.multiply(2)  # Added closing parenthesis
 print("After multiplying by 2:", calc.value)
 
-calc.divide(0
+calc.divide(0)  # Added closing parenthesis
 print("After attempting to divide by 0:", calc.value)
 
-calc.reset(
+calc.reset()  # Added closing parenthesis
 print("After reset:", calc.value)
